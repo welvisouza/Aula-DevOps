904f5a1c218d947eb9ffa06435ed7411d16f1654
welvisouza <145875959+welvisouza@users.noreply.github.com>
2023-11-03 11:19:08 -0300

    Update gerador_senha.py

--- gerador_senha.py
+++ gerador_senha.py
@@ -2,8 +2,8 @@ import string
 import random
 
 def gerar_senha(tamanho=15):
-    tes = string.ascii_letters + string.digits + string.punctuation
-    senha = ''.join(random.choice(caracteres) for _ in range(tamanho))
+    caracteres = string.ascii_letters + string.digits + string.punctuation
+    senha = ''.(random.choice(caracteres) for _ in range(tamanho))
     return senha
 
 # Exemplo de uso para gerar uma senha de tamanho 12
