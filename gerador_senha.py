6e56cde4a917595da0dfbbf04a0efdc30fe305e7
welvisouza <145875959+welvisouza@users.noreply.github.com>
2023-11-03 11:14:20 -0300

    Update gerador_senha.py

--- gerador_senha.py
+++ gerador_senha.py
@@ -2,7 +2,7 @@ import string
 import random
 
 def gerar_senha(tamanho=15):
-    caracteres = string.ascii_letters + string.digits + string.punctuation
+     = string.ascii_letters + string.digits + string.punctuation
     senha = ''.join(random.choice(caracteres) for _ in range(tamanho))
     return senha
 
